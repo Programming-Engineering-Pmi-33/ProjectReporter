5ea59b67f5cca9c9987c7e68fdd430a61c72d321
Roman Yanchuk <roman@cquelle.de>
2021-03-18 00:46:12 +0200

    #1 - Add collation for Ukrainian language to container.

--- build/local/docker/init.sql
+++ build/local/docker/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS project_reporter;
+CREATE DATABASE IF NOT EXISTS project_reporter CHARACTER SET koi8u_bin COLLATE koi8u_general_ci;
 DROP USER IF EXISTS 'developer'@'%';
 CREATE USER 'developer'@'%' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON project_reporter.* TO 'developer'@'%';
