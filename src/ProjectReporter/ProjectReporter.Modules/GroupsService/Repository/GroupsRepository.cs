61170721bab91d92b92e496f599ee88b0ae2ffaf
Roman Yanchuk <roman@cquelle.de>
2021-03-25 02:19:02 +0200

    #6 - Add prototype of Repository.

---
+++ src/ProjectReporter/ProjectReporter.Modules/GroupsService/Repository/GroupsRepository.cs
@@ -0,0 +1,13 @@
+﻿using ProjectReporter.Modules.GroupsService.Repository.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace ProjectReporter.Modules.GroupsService.Repository
+{
+    public class GroupsRepository : IGroupsRepository
+    {
+        public Task AddGroup(Group group)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/ProjectReporter/ProjectReporter.Modules/GroupsService/Repository/IGroupsRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProjectReporter.Modules.GroupsService.Repository.Models;
+using Task = System.Threading.Tasks.Task;
+
+namespace ProjectReporter.Modules.GroupsService.Repository
+{
+    public interface IGroupsRepository
+    {
+        Task AddGroup(Group group);
+    }
+}
