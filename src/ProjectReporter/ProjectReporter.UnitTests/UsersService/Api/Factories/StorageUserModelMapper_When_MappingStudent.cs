18266daa3d575c15017d77d771d95257bc1671d7
Roman Yanchuk <roman@cquelle.de>
2021-04-10 16:51:51 +0300

    #12 - Add unit tests for Factories.

---
+++ src/ProjectReporter/ProjectReporter.UnitTests/UsersService/Api/Factories/StorageUserModelMapper_When_MappingStudent.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ProjectReporter.Modules.UsersService.Api.Contracts;
+using ProjectReporter.Modules.UsersService.Api.Factories;
+using ProjectReporter.Modules.UsersService.Storage;
+
+namespace ProjectReporter.UnitTests.UsersService.Api.Factories
+{
+    [TestFixture]
+    public class StorageUserModelMapper_When_MappingStudent
+    {
+        [Test]
+        public void Then_StorageStudentReturned()
+        {
+            // Arrange
+            var contract = new StudentRegisterContract()
+            {
+                Email = "student@example.com",
+                FirstName = "name",
+                LastName = "surname",
+                GitLink = "gitLink",
+                GroupId = 1
+            };
+            var modelMapper = new StorageUserModelMapper();
+
+            // Act
+            var result = modelMapper.Map(contract);
+
+            // Assert
+            result.LastName.Should().Be("surname");
+            result.FirstName.Should().Be("name");
+            result.Email.Should().Be("student@example.com");
+            result.UserName.Should().Be("student@example.com");
+            result.GitLink.Should().Be("gitLink");
+        }
+    }
+}
