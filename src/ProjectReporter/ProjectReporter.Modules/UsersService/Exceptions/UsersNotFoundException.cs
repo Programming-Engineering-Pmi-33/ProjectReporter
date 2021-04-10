a12e8e9f6ce5c65af8fbc951602faf07c7ae5778
Roman Yanchuk <roman@cquelle.de>
2021-04-10 16:49:59 +0300

    #12 - Add validation for UsersRepository. Add UsersService exceptions.

---
+++ src/ProjectReporter/ProjectReporter.Modules/UsersService/Exceptions/UsersNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace ProjectReporter.Modules.UsersService.Exceptions
+{
+    public class UsersNotFoundException : UsersServiceException
+    {
+        public string[] UsersIds { get; }
+
+        public UsersNotFoundException(params string[] ids) : base(
+            $"User{(ids.Length > 0 ? "s" : "")} with id{(ids.Length > 0 ? "s" : string.Empty)} {ids.Select(i => i.ToString()).Join()} {(ids.Length > 0 ? " are" : " is")} not found.")
+        {
+            UsersIds = ids;
+        }
+
+        public UsersNotFoundException() { }
+    }
+}
--- src/ProjectReporter/ProjectReporter.Modules/UsersService/Repository/UsersRepository.cs
+++ src/ProjectReporter/ProjectReporter.Modules/UsersService/Repository/UsersRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ProjectReporter.Modules.UsersService.Exceptions;
 using ProjectReporter.Modules.UsersService.Repository.Factories;
 using ProjectReporter.Modules.UsersService.Storage;
 using Student = ProjectReporter.Modules.UsersService.Repository.Models.Student;
@@ -23,6 +24,10 @@ namespace ProjectReporter.Modules.UsersService.Repository
         public async Task<User[]> GetUsers(params string[] ids)
         {
             var users = await _storage.Users.Where(u => ids.Contains(u.Id)).ToArrayAsync();
+            if (users.Length != ids.Length)
+            {
+                throw new UsersNotFoundException(ids.Where(i => users.All(u => u.Id != i)).ToArray());
+            }
             return users.Select(u => _reconstructionFactory.Create(u)).ToArray();
         }
 
@@ -37,7 +42,10 @@ namespace ProjectReporter.Modules.UsersService.Repository
             var teachers = _storage.Teachers.Include(t => t.Department)
                 .Where(t => t.Department.FacultyId == facultyId);
             teachers = ids == null ? teachers : teachers.Where(t => ids.Contains(t.Id));
-
+            if (ids != null && ids.Length != 0 && teachers.Count() != ids.Length)
+            {
+                throw new UsersNotFoundException(ids.Where(i => teachers.All(u => u.Id != i)).ToArray());
+            }
             return (await teachers.ToArrayAsync()).Select(t => _reconstructionFactory.Create(t)).ToArray();
         }
     }
